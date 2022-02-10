b0c4f87443cb4481867e7847e211f03147f8fac7
Lucas es Lucas <midireccioneslucas@hotmail.com>
2022-02-10 00:07:28 -0300

    subida_06: componente Comic reemplazado por Item. Funcionalidades agregadas: Fetch con setTimeout, para simular demora del request. Respuesta (array) capturada como estado del ItemList. Mapeo de items para armar el catálogo

---
+++ src/components/Item/Item.js
@@ -0,0 +1,25 @@
+import styles from './Item.module.css';
+import ItemCount from "../ItemCount/ItemCount";
+
+const Item = (props) => {
+     return (
+          <>
+          <article className={styles.comic__container}>
+               <div className={styles.comic__cover}>
+               <img src={props.cover} width={props.cover_size} alt='cover'></img>
+                    <h3>{props.title}</h3>
+               </div>
+               <div className={styles.comic__info}>
+                    <p>Editorial: {props.editorial}</p>
+                    <h5>$ {props.price}</h5>
+                    <p>Ejemplares disponibles {props.stock}</p>
+               </div>
+               <div className={styles.comic__cart}>
+                    <ItemCount stock={props.stock} initial={1}></ItemCount>
+               </div>
+          </article>
+          </>
+     );
+}
+
+export default Item;
